723e64a4a8332b7478d2ed4c730f23bb62159d33
Devloper-solaiman <devlopersolaiman@gmail.com>
2025-06-27 15:52:36 +0600

    text color chainge

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
   },
   description: siteConfig.description,
   icons: {
-    icon: "/favicon.ico",
+    icon: "https://i.ibb.co/ksf6gj6Q/phone.jpg",
   },
 };
 
